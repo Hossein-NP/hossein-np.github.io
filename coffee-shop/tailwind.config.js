9fba3f3c9332cf1ee8cb62786a7e91e20111d7b4
Hossein <hosseinnoruzpur@outlook.com>
2023-11-30 17:39:30 +0330

    coffee-01

---
+++ coffee-shop/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // colors
+      colors:{
+        "brown":{
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        }
+      },
+      // shadow
+      boxShadow: {
+        "light" : "0px 1px 10px rgba(0,0,0,0.05)",
+      },
+      // borderradius
+      borderRadius: {
+        "4xl" : "2rem",
+      }
+    },
+  },
+  plugins: [],
+}
+
