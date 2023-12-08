f3c9339018856cea80e13a20825bff1e7864dc16
Hossein <hosseinnoruzpur@outlook.com>
2023-12-08 11:21:01 +0330

    coffee-02

--- coffee-shop/tailwind.config.js
+++ coffee-shop/tailwind.config.js
@@ -4,24 +4,32 @@ module.exports = {
   theme: {
     extend: {
       // colors
-      colors:{
-        "brown":{
+      colors: {
+        brown: {
           100: "#ECE0D1",
           300: "#DBC1AC",
           600: "#967259",
           900: "#634832",
-        }
+        },
       },
       // shadow
       boxShadow: {
-        "light" : "0px 1px 10px rgba(0,0,0,0.05)",
+        light: "0px 1px 10px rgba(0,0,0,0.05)",
       },
       // borderradius
       borderRadius: {
-        "4xl" : "2rem",
-      }
+        "4xl": "2rem",
+      },
+      // fontFamily
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemiBold: "Dana DemiBold",
+        " MorabbaLight": "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
     },
   },
   plugins: [],
-}
-
+};
